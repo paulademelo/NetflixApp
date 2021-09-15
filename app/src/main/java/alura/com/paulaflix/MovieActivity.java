3a5142ac7e0768613031084d75adce970b555865
paulademelo <paula.melo01@fatec.sp.gov.br>
2021-09-15 14:01:12 -0300

    Imagens

--- app/src/main/java/alura/com/paulaflix/MovieActivity.java
+++ app/src/main/java/alura/com/paulaflix/MovieActivity.java
@@ -1,9 +1,13 @@
 package alura.com.paulaflix;
 
 
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
 
 public class MovieActivity extends AppCompatActivity {
     @Override
@@ -17,6 +21,17 @@ public class MovieActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24);
+        }
+
+        // pegando a referencia do drawable
+        LayerDrawable drawable = (LayerDrawable) ContextCompat.getDrawable(this, R.drawable.shadows);
+
+        // verificando o null pointer exception
+        if (drawable != null){
+            Drawable movieCover = ContextCompat.getDrawable(this, R.drawable.movie_4);
+            drawable.setDrawableByLayerId(R.id.cover_drawable, movieCover);
+            // acessa o image view e troca a imagem atribuida
+            ((ImageView) findViewById(R.id.image_view_cover)).setImageDrawable(drawable);
         }
     }
 }
